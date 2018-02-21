f6c250f388834a6a8c1307cc244a6fd5371109fa
Luis Lozoya <lozoyaluis@hotmail.com>
2018-02-20 17:18:23 -0700

    iniciando seed tratando de unificar a primer rama

---
+++ database/seeds/ProfessionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+//se importo
+use Illuminate\Support\Facades\DB;
+
+class ProfessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //\Illuminate\Support\Facades\DB::;
+        DB::table('professions')->insert([
+           'name' => 'Desarrollador back-end',
+        ]);
+    }
+}
